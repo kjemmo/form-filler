48e97ce4b9c6566de94a1cf9d209105186ed9d5d
Morten Kjems <mortenkjems@gmail.com>
2019-11-09 21:53:26 +0100

    initial commit

---
+++ content-scripts/content.js
@@ -0,0 +1,8 @@
+/*
+We want to display a simple alert when the button with id #sidebar-button is clicked
+
+alert('content script executed!');
+
+*/
+
+document.querySelector('[name="username"]').value="Daniel345"
