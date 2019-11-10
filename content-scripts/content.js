fe3b5fdf887a78563d651c0918f2232a26b7c18f
Dmitry Khodarenok <dkhodarenok@gmail.com>
2019-11-10 19:53:09 +0700

    comments

--- content-scripts/content.js
+++ content-scripts/content.js
@@ -1,7 +1,8 @@
-// we want to fill in the input field like this, when the sidebar button is clicked:
 
-chrome.runtime.onMessage.addListener(function (msg,sender) {
-	if(msg.action == "fillForm"){
+//chrome.runtime.onMessage allows to communicate different parts of extension(content scripts, popups/sidebars , background)
+chrome.runtime.onMessage.addListener(function (msg) {//first parameter is a JSON received from some sender(sidebar in our case)
+	if(msg.action == "fillForm"){//just some flag that we need "fill" action
+		// we want to fill in the input field like this, when the sidebar button is clicked:
 		document.querySelector('[name="username"]').value="myusername";
 	}
 });
--- sidebar/sidebar.js
+++ sidebar/sidebar.js
@@ -1,7 +1,7 @@
-document.getElementById("sidebar-button").addEventListener("click",function(){
-	browser.tabs.query({active:true,currentWindow:true},function(tabs){
-		if(tabs && tabs.length >0){
-			browser.tabs.sendMessage(tabs[0].id,{action:"fillForm"})
+document.getElementById("sidebar-button").addEventListener("click",function(){//adding click handler to our button
+	browser.tabs.query({active:true,currentWindow:true},function(tabs){//querying active tab(active tab in current active window)
+		if(tabs && tabs.length >0){ //if tab found, in rare cases/bugs it can be empty
+			browser.tabs.sendMessage(tabs[0].id,{action:"fillForm"})// sending JSON with our "fill flag" to content script in this tab (tab.id)
 		}
 	})
 });
